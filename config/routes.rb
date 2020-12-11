1df182533ef2b05b7db78079356ab2d256438873
yoshiya-03 <yoshiya03y07@gmail.com>
2020-12-11 03:42:51 +0000

    [Update]画像投稿機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+ root 'post_images#index'
   devise_for :users
-  root to: 'homes#top'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
