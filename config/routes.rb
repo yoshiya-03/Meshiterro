2644a6fde8dee401468444ed20249bbaef174ab3
yoshiya-03 <yoshiya03y07@gmail.com>
2020-12-11 04:57:43 +0000

    [Update]コメント機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'post_images#index'
   devise_for :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+  resources :post_comments, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
