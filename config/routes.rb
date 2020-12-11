46dedd1957e0873ea45677e40bfc782e68e00443
yoshiya-03 <yoshiya03y07@gmail.com>
2020-12-11 06:20:12 +0000

    [Update]いいね機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root 'post_images#index'
+  get 'users/show'
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-  resources :post_comments, only: [:create, :destroy]
+  root 'post_images#index'
+  resources :post_images, only: [:new, :create, :index, :show] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
 end
